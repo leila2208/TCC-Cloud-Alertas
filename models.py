90b639366f842b4dc915df41801250f5b97d7064
leila2208 <leila22082007@gmail.com>
2025-08-19 17:11:01 -0300

    Update models.py

--- models.py
+++ models.py
@@ -3,26 +3,23 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-# Roles de usuario
-SUPERADMIN = "superadmin"
-
 class Hospital(db.Model):
-    __tablename__ = "hospitals"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), unique=True, nullable=False)
-    users = db.relationship("User", backref="hospital", lazy=True)
+    name = db.Column(db.String(150), unique=True, nullable=False)
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
 
 class User(db.Model):
-    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
-    role = db.Column(db.String(50), nullable=False)
-    hospital_id = db.Column(db.Integer, db.ForeignKey("hospitals.id"))
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    role = db.relationship('Role')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-
+    
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
