7386864412254d041973ba45f3195c546c9c3d5a
leila2208 <leila22082007@gmail.com>
2025-08-19 18:50:20 -0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "clave_por_defecto"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or "sqlite:///tcc.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
