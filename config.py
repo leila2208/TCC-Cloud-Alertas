5023463d1ae38f109549558ec2098c9f953c3997
leila2208 <leila22082007@gmail.com>
2025-08-19 17:46:32 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "tcc_secret_2024")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///tcc.db").replace("postgres://", "postgresql://")
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "clave_por_defecto"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or "sqlite:///tcc.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt_supersecret_2024")
-    SWAGGER_UI_DOC_EXPANSION = "list"
-    RESTX_VALIDATE = True
-    RESTX_MASK_SWAGGER = False
-    ERROR_404_HELP = False
