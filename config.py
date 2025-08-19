fbe6c31307d17f3daf929ec56cb99c2fc51c6a64
leila2208 <leila22082007@gmail.com>
2025-08-19 16:57:33 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "tcc_secret_2024")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///tcc.db").replace("postgres://", "postgresql://")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", "sqlite:///tcc.db"
+    ).replace("postgres://", "postgresql://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # para CORS o futuras integraciones
+    # Para CORS o futuras integraciones
     CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*")
