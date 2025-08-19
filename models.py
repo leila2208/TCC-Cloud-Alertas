0d81b9b4f6683bee4cc8a8db87bf36ddd80fb68d
leila2208 <leila22082007@gmail.com>
2025-08-19 16:05:25 -0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "tcc_secret_2024")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///tcc.db").replace("postgres://", "postgresql://")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # para CORS o futuras integraciones
+    CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*")
