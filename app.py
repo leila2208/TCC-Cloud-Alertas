e7aff2baedbde8ebb833921eca3562ff4dcb7c29
leila2208 <leila22082007@gmail.com>
2025-08-18 21:09:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template, redirect, url_for, s
 from flask_cors import CORS
 import copy
 
-app = Flask(_name_)
+app = Flask(__name__)
 CORS(app)
 app.secret_key = "supersecretkey"
 
@@ -74,4 +74,5 @@ def reset_tareas():
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
+
 
