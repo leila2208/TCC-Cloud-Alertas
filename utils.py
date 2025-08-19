8c94751b240412c823ff1c9b62bbe42bcea13ed1
leila2208 <leila22082007@gmail.com>
2025-08-19 17:53:23 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,2 @@
-from flask import abort
-from flask_login import current_user
-from models import SUPERADMIN, ADMIN, NURSE, PATIENT
-
-def require_roles(*roles):
-    def wrapper(func):
-        def inner(*args, **kwargs):
-            if not current_user.is_authenticated:
-                abort(401)
-            if current_user.role not in roles:
-                abort(403)
-            return func(*args, **kwargs)
-        inner.__name__ = func.__name__
-        return inner
-    return wrapper
+def format_datetime(dt):
+    return dt.strftime("%d/%m/%Y %H:%M")
