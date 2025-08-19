a0245d4422bd881fc861b5e0e67078f43bc78b3b
leila2208 <leila22082007@gmail.com>
2025-08-19 18:46:48 -0300

    Delete static directory

--- static/alertas.js
+++
@@ -1,9 +0,0 @@
-function attendAlert(alertId) {
-    fetch(/api/alert/${alertId}/attend, { method: 'POST' })
-        .then(res => res.json())
-        .then(data => {
-            if(data.success){
-                document.getElementById(alert-${alertId}).remove();
-            }
-        });
-}
